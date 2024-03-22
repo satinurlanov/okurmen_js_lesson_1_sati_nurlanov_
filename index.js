911036bdc9d2ed11d7858263e8e030f2b1b12a1e
satar nurlan uulu <umarfaaruq2019@gmail.com>
2024-03-22 09:44:42 +0600

    ls1

---
+++ index.js
@@ -0,0 +1,29 @@
+//alert('hello world');
+// const year = prompt('how old are you','');
+// alert('i am ' + year);
+
+// const areyoudev = confirm('Are you web dev?');
+// alert(areyoudev)
+
+
+// let name = 'sati';
+// alert('i am ' + name)
+
+// let name = 'sati';
+// alert('i am '+ name)
+// const a = 2;
+// const b = 4;
+// const c = 2;
+// const total = a + b / c; 
+// alert(total)
+const firstname = 'sati';
+const position = 'Web dev'; 
+//alert(firstname + '  ' + position);
+//alert( 'names length' + firstname.length + 'djsk')
+//alert(position.slice(0,4));
+//alert('dwasx ' + firstname.toUpperCase ());
+// touppercese - chon tamga m/n jazylat
+// tolowercase - kichinekei tamga m/n jazylat
+const string = 'sa, sati, how are oyu';
+const stringRight = string[0].toUpperCase() + string[1].toLowerCase() + ', ' + string[4].toUpperCase() + string.slice(5).toLowerCase() + ' ? ';
+alert(stringRight);
